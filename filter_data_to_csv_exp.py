2bf596007b6244f28008f22ee0e9371b33f1819b
taktam <teslalinux@gmail.com>
2021-11-25 17:37:10 +0100

    export form

--- filter_data_to_csv_exp.py
+++ filter_data_to_csv_exp.py
@@ -1,11 +1,15 @@
+from os import close
 import pandas
 import csv
 import fnmatch
 
-file_name = "feeders.html"
+open_file_name = "feeders.html"
+save_file_name = "feeders.csv"
 
-data = pandas.read_html(file_name)
-
+data = pandas.read_html(open_file_name)
+file_to_save = open (save_file_name, 'w')
+writer = csv.writer(file_to_save)
+list_to_write = []
 fdr_name = None
 sap = 0
 
@@ -17,4 +21,11 @@ for i in range(len(data[0])):
             if fnmatch.fnmatch(str(data[0].iloc[i,3]), 'Smart*')!=True:
                 if fnmatch.fnmatch(str(data[0].iloc[i,3]), 'Stationary*')!=True:
                     if fnmatch.fnmatch(str(data[0].iloc[i,3]), 'Tape Strips*')!=True:
+                        list_to_write.clear
+                        list_to_write.append [str(fdr_name)]
+                        list_to_write.append [str(data[0].iloc[i,3])]
+                        list_to_write.append [str(data[0].iloc[i,5])]
                         print (fdr_name,',', str(data[0].iloc[i,3])+'\t'+','+str(data[0].iloc[i,5]))
+                        writer.writerows(list_to_write)
+file_to_save.close
+
